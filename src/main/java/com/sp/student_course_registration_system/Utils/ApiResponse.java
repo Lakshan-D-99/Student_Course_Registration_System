a5bc680c3b62f20d59791ffbb30c23d958abc7b4
Dhanuka <lakshandhanuka398@gmail.com>
2025-12-28 20:52:43 +0100

    Configured ApiResponse class to share api information between APIs.

---
+++ src/main/java/com/sp/student_course_registration_system/Utils/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.sp.student_course_registration_system.Utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ApiResponse {
+    private String message;
+}
