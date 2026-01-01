f4c83076a70dad35684bfb3c9d39a1f505907e5b
Dhanuka <lakshandhanuka398@gmail.com>
2026-01-01 15:53:10 +0100

    This is the final commit of this Project.

--- src/main/java/com/sp/student_course_registration_system/StudentCourseRegistrationSystemApplication.java
+++ src/main/java/com/sp/student_course_registration_system/StudentCourseRegistrationSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentCourseRegistrationSystemApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StudentCourseRegistrationSystemApplication.class, args);
     }
